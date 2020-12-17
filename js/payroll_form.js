1dad243a77e2d1aa761062015031f12241240ab2
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-17 19:52:53 +0530

    [Prudhvi] add . adding button styles

---
+++ js/payroll_form.js
@@ -0,0 +1,18 @@
+function cancelHover() {
+    document.getElementById("cancel").className="cancelButtonOnHover";
+}
+function cancelHoverOut() {
+    document.getElementById("cancel").className="btn";
+}
+function submitHover() {
+    document.getElementById("submitButton").className="submitButtonOnHover";
+}
+function submitHoverOut() {
+    document.getElementById("submitButton").className="submitButton";
+}
+function resetHover() {
+    document.getElementById("resetButton").className="resetButtonOnHover";
+}
+function resetHoverOut() {
+    document.getElementById("resetButton").className="resetButton";
+}
