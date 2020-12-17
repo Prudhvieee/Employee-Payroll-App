00d0545c038c1aee03509ad1803d1be0c7a0d494
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-17 21:35:04 +0530

    [Prudhvi] add . adding event to set salary

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,18 +1,25 @@
 function cancelHover() {
-    document.getElementById("cancel").className="cancelButtonOnHover";
+  document.getElementById("cancel").className = "cancelButtonOnHover";
 }
 function cancelHoverOut() {
-    document.getElementById("cancel").className="btn";
+  document.getElementById("cancel").className = "btn";
 }
 function submitHover() {
-    document.getElementById("submitButton").className="submitButtonOnHover";
+  document.getElementById("submitButton").className = "submitButtonOnHover";
 }
 function submitHoverOut() {
-    document.getElementById("submitButton").className="submitButton";
+  document.getElementById("submitButton").className = "submitButton";
 }
 function resetHover() {
-    document.getElementById("resetButton").className="resetButtonOnHover";
+  document.getElementById("resetButton").className = "resetButtonOnHover";
 }
 function resetHoverOut() {
-    document.getElementById("resetButton").className="resetButton";
+  document.getElementById("resetButton").className = "resetButton";
 }
+// Event listener to select salary
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function () {
+  output.textContent = salary.value;
+});
